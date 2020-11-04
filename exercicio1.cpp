ace6dd4b111c7b22daa165e6cbc50d6483ff92bf
Robinson <robinpaulista@yahoo.com.br>
2020-11-04 17:36:46 -0300

    Exercícios de fixação.

--- exercicio1.cpp
+++ exercicio1.cpp
@@ -1,2 +1,19 @@
-/* 3.7. Faça um programa que peça dois números inteiros. Imprima a soma desses dois
+/* 1. Faça um programa que peça dois números inteiros. Imprima a soma desses dois
 números na tela. */
+
+#include <stdio.h>
+int main(){
+    int n1, n2, soma;
+    printf("Declare dois numeros inteiros: \n");
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+
+    soma = n1 + n2;
+
+    printf("A soma desses dois numeros sera: %d \n", soma);
+
+    getchar();
+    return 0;
+
+}
+
--- exercicio2.cpp
+++ exercicio2.cpp
@@ -1,2 +1,16 @@
-/* 3.8. Escreva um programa que leia um valor em metros e o exiba convertidos
+/* 2. Escreva um programa que leia um valor em metros e o exiba convertidos
  em milímetros.*/
+
+#include <stdio.h>
+#include<math.h>
+
+int main(){
+    float metro;
+    printf("Declare um valor em metros\n");
+    scanf("%f", &metro);
+    
+      metro = metro * 1000;
+    
+    printf("O valor declarado em metros equivale a: %.2f milimetros \n", metro);
+    
+}
